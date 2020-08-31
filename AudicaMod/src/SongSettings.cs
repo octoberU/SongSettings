48579bbc3281f5faab44b9936a5cc89384f13b9c
octo <allblvnk@gmail.com>
2020-08-31 17:23:20 +0100

    SongSettings prep

---
+++ AudicaMod/src/SongSettings.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using MelonLoader;
+using UnityEngine;
+
+namespace AudicaModding
+{
+    public class AudicaMod : MelonMod
+    {
+        public static class BuildInfo
+        {
+            public const string Name = "SongSettings";  // Name of the Mod.  (MUST BE SET)
+            public const string Author = "octo"; // Author of the Mod.  (Set as null if none)
+            public const string Company = null; // Company that made the Mod.  (Set as null if none)
+            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        }
+
+    }
+}
+
+
+
